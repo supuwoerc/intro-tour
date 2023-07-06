aac8d5252e73f297218dd08ba6b5a0369e376856
zhangqm <qimeng_zhang@intsig.net>
2023-07-06 21:43:08 +0800

    feat: 初始化脚手架

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export const TEMPLATE_INFO = '使用GithubActions&TypeDoc部署文档'
+/**
+ * @param a 数字1
+ * @param b 数字2
+ * @returns 数字1和数字2的求和结果
+ */
+export function addNumber(a: number, b: number) {
+    return a + b
+}
