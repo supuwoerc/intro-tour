0b72e4d4a10c9c9531657b900658bf9429fdec1d
zhangqm <qimeng_zhang@intsig.net>
2023-07-15 23:56:27 +0800

    feat: 添加domParse方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,18 @@
 export function uuid() {
     return crypto.randomUUID()
 }
+
+/**
+ * 将字符串模板转为dom
+ * @param templates 字符串模板
+ * @returns DocumentFragment
+ */
+export function domParse(...templates: string[]) {
+    const documentFragment = document.createDocumentFragment()
+    for (let index = 0; index < templates.length; index += 1) {
+        const template = templates[index]
+        const contextualFraement = document.createRange().createContextualFragment(template)
+        documentFragment.appendChild(contextualFraement)
+    }
+    return documentFragment
+}
